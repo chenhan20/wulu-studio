7a1421c70d1c14dd7d695a59dbe22ee19344033e
steve <a0911558945@gmail.com>
2018-02-02 18:07:48 +0800

    20180202 commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,13 @@
-var app = new Vue({
-    el: '#app',
+var app_1 = new Vue({
+    el: '#app_1',
     data: {
       message: 'Hello Vue!'
+    }
+  })
+
+  var app_2 = new Vue({
+    el: '#app_2',
+    data: {
+      checkedNames: []
     }
   })
