1634dc212eea3f5ed48c39a51f68d5d8e6e85614
steve <a0911558945@gmail.com>
2018-02-26 00:37:50 +0800

    20180225commit

--- public/js/index.js
+++ public/js/index.js
@@ -7,22 +7,8 @@
  * 第二位數字為 分區代表 五區:1四區:2三區:3...  依此類推
  * 第三位數字為 分數代表 0~19:1,20-39:2,40-59:3...依此類推  --分數先不建(感覺有更好的方法)
  * 　
-　１,２００元　
-　１,２００元
-　１,４００元　
-　１,４００元
-１,６００元
-１,４００元
-１,６００元
-２,０００元
-２,０００元
-２,４００
-　　３,０００
-　　３,５００
-　　４,０００
-　　４,０００
-♬　大師每勝差１,２００元起。
-♬　菁英每勝差１,６００元起。
+ *♬　大師每勝差１,２００元起。
+ *♬　菁英每勝差１,６００元起。
 
  */
 var price_total = {11:0,12:700,13:1400,14:2100,15:2800,          //銅牌
@@ -32,8 +18,13 @@ var price_total = {11:0,12:700,13:1400,14:2100,15:2800,          //銅牌
                    51:24450,52:27450,53:30950,54:34950,55:38950, //鑽石
                    61:44950                                      //大師
 }
-var price_point = {
-
+//區段勝差對應價錢
+var price_point = {11:120,12:120,13:120,14:120,15:120,  //銅牌
+                   21:140,22:140,23:140,24:140,25:140,  //銀牌
+                   31:180,32:180,33:200,34:200,35:220,  //金牌
+                   41:250,42:250,43:300,44:300,45:300,  //白金
+                   51:500,52:500,53:600,54:600,55:800,  //鑽石
+                   61:1200,71:1400                      //大師、菁英
 }
 
 
@@ -48,35 +39,34 @@ var price = new Vue({
       //原先分數與牌位與金額
       var original_league=this.original_league;
       var original_area=this.original_area;
-      var original_point=this.original_point;
+      var original_point=parseInt(this.original_point);
       //目標分數與牌位與金額
       var target_league=this.target_league;
       var target_area=this.target_area;
-      var target_point=this.target_point;
+      var target_point=parseInt(this.target_point);
 
-      var original_league_Num=league_num(original_league);
-      var original_area_num=area_num(original_area);
-      var original_point_num=point_num(original_point);
-      var target_league_Num=league_num(target_league);
-      var target_area_num=area_num(target_area);
-      var target_point_num=point_num(target_point); 
+      var original_league_Num=parseInt(league_num(original_league));
+      var original_area_num=parseInt(area_num(original_area));
+      var original_point_num=parseInt(point_num(original_point));
+      var target_league_Num=parseInt(league_num(target_league));
+      var target_area_num=parseInt(area_num(target_area));
+      var target_point_num=parseInt(point_num(target_point)); 
 
       //組合字串
       var og_league_area=''.concat(original_league_Num,original_area_num);
       var tg_league_area=''.concat(target_league_Num,target_area_num);
 
-      console.log('tg_league_area:'+tg_league_area);
+
       totalPrice=price_total[tg_league_area]-price_total[og_league_area];
       
       //若分數不為零則需判斷分數  此為原始分數 須扣除原先勝差的金額
       if(original_point_num!=0){
-        totalPrice-=100;  //先這樣
+        totalPrice-=price_point[og_league_area]*original_point_num;  
       }
       //若分數不為零則需判斷分數  此為目標分數 須加上多買分數勝差的金額
       if(target_point_num!=0){
-        totalPrice+=100;  //先這樣
+        totalPrice+=price_point[tg_league_area]*target_point_num;  
       }
-
 
       return totalPrice;
     },
@@ -91,29 +81,29 @@ var price = new Vue({
       //原先分數與牌位與金額
       var original_league=this.original_league;
       var original_area=this.original_area;
-      var original_point=this.original_point;
+      var original_point=parseInt(this.original_point);
       //目標分數與牌位與金額
       var target_league=this.target_league;
       var target_area=this.target_area;
-      var target_point=this.target_point;
+      var target_point=parseInt(this.target_point);
       
 
-      var original_league_Num=league_num(original_league);
-      var target_league_Num=league_num(target_league);
-      var original_area_num=area_num(original_area);
-      var target_area_num=area_num(target_area);
+      var original_league_Num=parseInt(league_num(original_league));
+      var target_league_Num=parseInt(league_num(target_league));
+      var original_area_num=parseInt(area_num(original_area));
+      var target_area_num=parseInt(area_num(target_area));
 
       var og_league_area=''.concat(original_league_Num,original_area_num);
       var tg_league_area=''.concat(target_league_Num,target_area_num);
-
+      debugger;
       // alert(og_league_area);
       if(og_league_area>tg_league_area){
         //牌位不符合直接排除掉 
         return 'leagueORareaFail';
-      }else if(og_league_area==tg_league_area&&original_point>=target_point){
+      }else if(og_league_area==tg_league_area&&original_point==target_point){
         //接著判斷相同牌位 相同分區 就要判斷分數
         return 'pointSame';
-      }else if(original_point>100||original_point<0||target_point>100||target_point<0){
+      }else if(original_point>100||original_point<0||target_point>100||target_point<0||(original_point>target_point&&og_league_area==tg_league_area)){
         return 'pointFail';
       }else{
         return 'success';
